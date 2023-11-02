a4d43b7b3883b80d84db9071c653c16cd1035b2a
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-11-02 16:10:22 +1300

    Start writing new des controller

--- controller/DES/new_des.py
+++ controller/DES/new_des.py
@@ -9,7 +9,11 @@ from view.data_explorer_view import DES_View
 def accept(event, values, state):
     keep_going = True
     if event == '-OTHER_DES-':
-        des_obj = DES_View()
-        des_obj.set_up_layout('des')
-        des_obj.render()
+        # Get selected user
+        des_owner = values['-DES_DROPDOWN-']
+        if des_owner != 'Select a DES':
+            # Create window
+            des_obj = DES_View()
+            des_obj.set_up_layout('other_des', des_owner)
+            des_obj.render()
     return keep_going 
