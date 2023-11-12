286c7431f80d08d38a3cb4238ea3bc51b42b33ef
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-11-13 10:50:40 +1300

    Update some of the module inits

--- view/__init__.py
+++ view/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["ChartExamples","data_explorer_view"]
+__all__ = ["ChartExamples","data_explorer_view", "data_explorer_controller", "data_explorer_model", "data_explorer_layout", "data_explorer_view"]
