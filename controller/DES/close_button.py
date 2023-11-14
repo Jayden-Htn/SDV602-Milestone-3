3a0fe3658f51febb576e19492d76bd4c83b35731
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-11-15 11:00:48 +1300

    Improve close button to end program when the des view is closed and there is no menu (because of testing)

--- controller/DES/close_button.py
+++ controller/DES/close_button.py
@@ -10,5 +10,8 @@ from view.window_view import Window_View
 def accept(event, values, state):
     if event in (sg.WIN_CLOSED, '-CLOSE-'):   
         state.window.close()
+        Window_View.view_list.remove(state)
+        # If main menu is not open, exit program
+        if len(Window_View.view_list) == 0:
+            sys.exit()
     return True
-       
