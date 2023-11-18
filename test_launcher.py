07cefcbd82c7f9678f839ea612eb4a92812fc4df
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-11-19 11:32:38 +1300

    Update test launcher for new testing

--- test_launcher.py
+++ test_launcher.py
@@ -18,8 +18,8 @@ if __name__ == "__main__" :
         Code that runs when this is the main module.
     """
     Window_View.set_theme()
-    Window_View.user = 'Sam'
-    des_obj = DES_View('Sam')
+    Window_View.user = 'Sam' # Logged in user
+    des_obj = DES_View('Sam') # Des owner
     des_obj.set_layout()
     des_obj.render()
     des_obj.set_data()
