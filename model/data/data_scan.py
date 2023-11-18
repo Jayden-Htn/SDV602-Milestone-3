4b3209e0adcb08bea0170575467e08db9f737240
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-11-19 11:44:25 +1300

    Clear chart info on clear all

--- model/data/data_scan.py
+++ model/data/data_scan.py
@@ -105,6 +105,23 @@ class Data_Manager():
         csv_file_obj = self.find_file(file_path+user+'.csv', 'w')
         csv_file_obj.write('')
         self.close_file(csv_file_obj)
+        # Clear info data
+        csv_file_obj = self.find_file(file_path+'des.csv', 'r+')
+        try:
+            lines = csv_file_obj.readlines()
+            for i in range(len(lines)):
+                this_line = lines[i].strip().split(',')
+                # If the user's name is found, update the chart info
+                if this_line[0] == user:
+                    lines[i] = user + ',' + '' + ',' + '' + '\n'
+                    break
+            # Write the updated lines to the csv file
+            csv_file_obj.seek(0)
+            csv_file_obj.writelines(lines)
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+        # Update remote info
+        # self.update_des_info(user, '', '')
     
 
     def write_csv(self, values_dict):
@@ -253,7 +270,6 @@ class Data_Manager():
         csv_file_obj = self.find_file(file_path+'des.csv')
         # Handle error
         if 'File Error' in self.status:
-            self.close_file(csv_file_obj)
             return None
         # Read each line of the csv file
         try:
@@ -279,10 +295,10 @@ class Data_Manager():
             title (str): The chart title.
             description (str): The chart description.
         """
+        found = False
         csv_file_obj = self.find_file(file_path+'des.csv', 'r+')
         # Handle error
         if 'File Error' in self.status:
-            self.close_file(csv_file_obj)
             return False
         # Read each line of the csv file
         try:
@@ -292,10 +308,14 @@ class Data_Manager():
                 # If the user's name is found, update the chart info
                 if this_line[0] == user:
                     lines[i] = user + ',' + title + ',' + description + '\n'
+                    found = True
                     break
             # Write the updated lines to the csv file
             csv_file_obj.seek(0)
             csv_file_obj.writelines(lines)
+            # If the user's name was not found, add a new line to end
+            if not found:
+                csv_file_obj.write(user + ',' + title + ',' + description + '\n')
         except:
             print("Unexpected error:", sys.exc_info()[0])
             return False
